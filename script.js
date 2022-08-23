204977aeec550810218b07fd04f9ed3270c5ae13
haybales <haleynicole126@gmail.com>
2022-08-22 21:29:32 -0600

    added javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&t=2s")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&t=2s")
+})
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=MvgiPlVF0Tg&t=2s")
+})
+document.getElementsByClassName("article")[0].addEventListener("click", function() {
+    window.open("https://dev.to/codefoxx/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function() {
+    window.open("https://dev.to/codefoxx/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function() {
+    window.open("https://dev.to/codefoxx/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj")
+})
